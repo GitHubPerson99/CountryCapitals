0d1ddc2cd0b739596e9ebf1a1b7075bf32403664
GitHubPerson99 <aarav.sheel.parida@gmail.com>
2022-04-20 11:03:29 +0100

    Country And Capital

---
+++ app/src/main/java/com/aacreations/countryandcapitals/SplashScreenActivity.java
@@ -0,0 +1,36 @@
+package com.aacreations.countryandcapitals;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class SplashScreenActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        ImageView layout = findViewById(R.id.splash_screen_image);
+        layout.setAlpha(0f);
+        layout.animate().setDuration(500).alpha(1f).withEndAction(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, FirstScreenActivity.class);
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                finish();
+            }
+        });
+
+    }
+
+}
