8c9df4ea221b2f791fc8bc76b7007a295cde96e5
GitHubPerson99 <aarav.sheel.parida@gmail.com>
2022-04-21 15:14:31 +0100

    Country Capital App

--- app/src/main/java/com/aacreations/countryandcapitals/SplashScreenActivity.java
+++ app/src/main/java/com/aacreations/countryandcapitals/SplashScreenActivity.java
@@ -9,6 +9,8 @@ import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
